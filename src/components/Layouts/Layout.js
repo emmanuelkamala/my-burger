ca42761d8f64c09b5f5ad61c0a9556a343f55bf6
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-25 10:18:37 +0300

    Creat a burger ingredient component

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -2,12 +2,12 @@ import React from 'react';
 import styles from './Layout.module.css';
 
 const layout = (props) => (
-    <React.Fragment>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={styles.content}>
-            {props.children}
-        </main>
-    </React.Fragment>
+  <React.Fragment>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={styles.content}>
+      {props.children}
+    </main>
+  </React.Fragment>
 )
 
 export default layout;

46b4d373d98cf77d4df84d57b223595549082c29
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-01-12 14:20:02 +0300

    Edit index.html file

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,6 +12,19 @@ class BurgerBuilder extends Component {
     }
   }
 
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    }
+    updatedIngredients[type] = updatedCount;
+  }
+
+  removeIngredientHandler = (type) => {
+
+  }
+
   render() {
     return (
       <React.Fragment>
